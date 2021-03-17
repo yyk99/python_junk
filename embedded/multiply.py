1790c796476007d90e1b49c5a26ab1d285622dd7
Yuri Kuznetsov <yyk@plein-2>
2021-03-17 13:31:43 -0400

    embedded - added one more example

---
+++ embedded/multiply.py
@@ -0,0 +1,9 @@
+#
+# see https://docs.python.org/3/extending/embedding.html#pure-embedding
+#
+def multiply(a,b):
+    print("Will compute", a, "times", b)
+    c = 0
+    for i in range(0, a):
+        c = c + b
+    return c
