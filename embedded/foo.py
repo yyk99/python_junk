21d2da7ded8fd8e94cf39659616804a3bc812a50
Yuri Kuznetsov <yyk@plein-2>
2021-03-17 14:30:38 -0400

    embedded - Updated for emd module

---
+++ embedded/foo.py
@@ -0,0 +1,9 @@
+#
+# test emb
+#
+
+import emb
+
+def foo(x,y):
+	print("Number of arguments: ", emb.numargs())
+	return  x + y
--- embedded/pure_embedding.cpp
+++ embedded/pure_embedding.cpp
@@ -4,6 +4,34 @@
 #define PY_SSIZE_T_CLEAN
 #include <Python.h>
 
+static int numargs = 0;
+
+/* Return the number of arguments of the application command line */
+static PyObject *
+emb_numargs( PyObject *self, PyObject *args )
+{
+    if ( !PyArg_ParseTuple( args, ":numargs" ) )
+        return NULL;
+    return PyLong_FromLong( numargs );
+}
+
+static PyMethodDef EmbMethods[] = {
+    {"numargs", emb_numargs, METH_VARARGS,
+     "Return the number of arguments received by the process."},
+    {NULL, NULL, 0, NULL}
+};
+
+static PyModuleDef EmbModule = {
+    PyModuleDef_HEAD_INIT, "emb", NULL, -1, EmbMethods,
+    NULL, NULL, NULL, NULL
+};
+
+static PyObject *
+PyInit_emb( void )
+{
+    return PyModule_Create( &EmbModule );
+}
+
 int
 main(int argc, char *argv[])
 {
@@ -16,12 +44,8 @@ main(int argc, char *argv[])
         return 1;
     }
 
-    {
-        if ( access( argv[1], 0 ) != 0 ) {
-            fprintf( stderr, "Cannot find file: %s\n", argv[1] );
-            return 1;
-        }
-    }
+    numargs = argc;
+    PyImport_AppendInittab( "emb", &PyInit_emb );
 
     Py_Initialize();
     PyRun_SimpleString( "import sys" );
