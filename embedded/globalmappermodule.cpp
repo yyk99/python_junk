b48b90dfac0c089cde15c5c999427250afd2ad3b
Yuri Y. Kuznetsov <yyk@kestrel>
2021-03-20 05:32:41 -0400

    embedded - Updated the test script

--- embedded/globalmappermodule.cpp
+++ embedded/globalmappermodule.cpp
@@ -465,6 +465,20 @@ main(int argc, char* argv[])
         "    gm.roj('hello there')\n"
         "except Exception as err:\n"
         "    print('Error: ', err)\n"
+        "o1 = gm.new()\n"
+/*
+    ['__class__', '__delattr__', '__dir__', '__doc__', '__eq__', '__format__', '__ge__', 
+     '__getattribute__', '__gt__', '__hash__', '__init__', '__init_subclass__', '__le__', 
+     '__lt__', '__ne__', '__new__', '__reduce__', '__reduce_ex__', '__repr__', '__setattr__', 
+     '__sizeof__', '__str__', '__subclasshook__', 'demo']
+*/
+        "print(dir(o1))\n"
+        "o1.attr1 = 'otsos'\n"
+        "print('o1.attr1 =', o1.attr1)\n"
+        "o1.attr2 = 'podsos'\n"
+        "print('o1.attr2 =', o1.attr2)\n"
+        "print(o1.__dir__())\n"
+        "# print(enumerate(o1,0))\n"
     );
 
     PyMem_RawFree(program);
